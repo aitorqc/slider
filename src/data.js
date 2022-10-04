576100815e424a15d4b79d088500dc89161e5210
aquicor <aquinoacordero@gmail.com>
2022-10-05 00:20:16 +0200

    v.1

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const people = [{
+        id: 1,
+        image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1664666198~exp=1664666798~hmac=1106d17125a35bd701314c3950833824e768d4c82a77d34424579ca3aae66ca0",
+        name: "maria ferguson",
+        title: "office manager",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+    },
+    {
+        id: 2,
+        image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1664666198~exp=1664666798~hmac=1106d17125a35bd701314c3950833824e768d4c82a77d34424579ca3aae66ca0",
+        name: "john doe",
+        title: "regular guy",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+    },
+    {
+        id: 3,
+        image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1664666198~exp=1664666798~hmac=1106d17125a35bd701314c3950833824e768d4c82a77d34424579ca3aae66ca0",
+        name: "maria ferguson",
+        title: "office manager",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+    },
+    {
+        id: 4,
+        image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1664666198~exp=1664666798~hmac=1106d17125a35bd701314c3950833824e768d4c82a77d34424579ca3aae66ca0",
+        name: "john doe",
+        title: "regular guy",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+    },
+    {
+        id: 5,
+        image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1664666198~exp=1664666798~hmac=1106d17125a35bd701314c3950833824e768d4c82a77d34424579ca3aae66ca0",
+        name: "maria ferguson",
+        title: "office manager",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+    },
+]
+
+export default people;
